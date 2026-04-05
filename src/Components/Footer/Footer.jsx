7e3aeb8c26be19ff1680eb3c7322abc526ff9f89
Md Mehedi Hasan Shanto <ll4146394@gmail.com>
2026-04-05 19:50:52 +0600

    first commit

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <p>
+        <small>Thanks For Visited.</small>
+        <a href="/">terms</a>
+        <a href="/">privacy</a>
+        <a href="/">facebook</a>
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/Components/Mobiles/Mobiles.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Mobiles = () => {
+  return (
+    <div>
+      <h4>This is Mobilses</h4>
+    </div>
+  );
+};
+
+export default Mobiles;
